ae7e71c304111b89d1f934bcd5e5e800526a3d2b
DereviankoYaroslav <yarik0009258@gmail.com>
2022-11-24 22:31:14 +0200

    angularCourse start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import {products as data} from './data/products'
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+
+  products: IProduct[] = []
+
+  loading = false;
+
+  constructor(private productsService: ProductService) {
+  }
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+      this.loading = false;
+    })
+  }
+
+
 }
